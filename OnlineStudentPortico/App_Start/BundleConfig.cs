574c66003f50678a2f2100710502e0babce2a3c9
shivani_patel <shivani.patel.fullstack01@gmail.com>
2019-03-19 00:14:05 +0530

    commit design

--- OnlineStudentPortico/App_Start/BundleConfig.cs
+++ OnlineStudentPortico/App_Start/BundleConfig.cs
@@ -8,20 +8,52 @@ namespace OnlineStudentPortico
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            #region Admin Side template
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/adminLayout/css").Include(
+                    "~/Content/AdminLayout/css/bootstrap.min.css",
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                    "~/Content/AdminLayout/css/metisMenu.min.css",
+
+                    "~/Content/AdminLayout/css/timeline.css",
+
+                    "~/Content/AdminLayout/css/startmin.css",
+
+                    "~/Content/AdminLayout/css/morris.css",
+
+                    "~/Content/AdminLayout/css/font-awesome.min.css.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/adminLayout/js").Include(
+                "~/Scripts/AdminLayout/js/jquery.min.js",
+                "~/Scripts/AdminLayout/js/bootstrap.min.js",
+                "~/Scripts/AdminLayout/js/metisMenu.min.js",
+                "~/Scripts/AdminLayout/js/raphael.min.js",
+                "~/Scripts/AdminLayout/js/morris.min.js",
+                "~/Scripts/AdminLayout/js/morris-data.min.js",
+                "~/Scripts/AdminLayout/js/startmin.js"
+
+                ));
+            #endregion
+
+
+
+
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
         }
     }
 }
---
+++ OnlineStudentPortico/Areas/administrator/Controllers/CountryManageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using OnlineStudentPortico.Models;
+
+namespace OnlineStudentPortico.Areas.administrator.Controllers
+{
+    public class CountryManageController : Controller
+    {
+        dbLocationEntities db = new dbLocationEntities();
+
+        // GET: administrator/CountryManage
+        public ActionResult Index()
+        {
+            return View(db.CountryMasters.ToList());
+        }
+
+       
+
+    }
+}
